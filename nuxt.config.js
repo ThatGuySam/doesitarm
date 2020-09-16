4ae327b2d648cdfbe6018dfde4959544476c0db2
Sam Carlton <sam@sam.lc>
2020-09-16 00:14:18 -0500

    Set app name

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ** Headers of the page
     */
     head: {
-        title: pkg.name,
+        title: 'Does it ARM',
         meta: [
             { charset: 'utf-8' },
             {
