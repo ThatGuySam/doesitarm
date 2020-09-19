58f118a9dc744aa4e96cb51d76cbea93e7626a3f
Sam Carlton <sam@sam.lc>
2020-09-19 15:40:25 -0500

    Set vue html indent to 4 spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "indent": ['error', 4]
+    'indent': ['error', 4],
+    'vue/html-indent': ['error', 4]
   }
 }
