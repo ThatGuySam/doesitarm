0c56276d096f473dec5a84506e7b56209c8ce4f8
Sam Carlton <sam@sam.lc>
2020-09-19 15:58:05 -0500

    Add app list to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,50 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        doesitarm
-      </h1>
-      <h2 class="subtitle">
-        Apps that support ARM on MacOs
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+    <section class="container">
+        <div>
+            <logo/>
+            <h1 class="title">
+                Does it ARM
+            </h1>
+            <h2 class="subtitle">
+                Apps that are reported to support ARM on macOS
+            </h2>
+            <div class="apps">
+                <div
+                    v-for="app in appList"
+                    :key="app.slug"
+                >
+                    <a :href="`/app/${app.slug}`">
+                        <div>
+                            {{ app.name }}
+                        </div>
+                    </a>
+                </div>
+                <a
+                    href="https://nuxtjs.org/"
+                    target="_blank"
+                    class="button--green">Documentation</a>
+                <a
+                    href="https://github.com/nuxt/nuxt.js"
+                    target="_blank"
+                    class="button--grey">GitHub</a>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 
+import appList from '~/assets/app-list.json'
+
 export default {
     components: {
         Logo
+    },
+    computed: {
+        appList() {
+            return appList
+        }
     }
 }
 </script>
