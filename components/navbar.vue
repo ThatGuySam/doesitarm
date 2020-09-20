13b6ef95890bb6bc32dcaaf04fe2eb868b6edf55
Sam Carlton <sam@sam.lc>
2020-09-19 22:07:50 -0500

    Make active menu item neumorphic

--- components/navbar.vue
+++ components/navbar.vue
@@ -50,7 +50,7 @@
                             :href="item.url"
                             :class="[
                                 'px-3 py-2 rounded-md text-sm font-medium leading-5 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out',
-                                ($nuxt.$route.path === item.url) ? 'text-white bg-gray-900 hover:text-white' : 'text-gray-300 hover:bg-darker hover:neumorphic-shadow'
+                                ($nuxt.$route.path === item.url) ? 'text-white bg-darker hover:text-white neumorphic-shadow' : 'text-gray-300 hover:bg-darker hover:neumorphic-shadow'
                             ]"
                         >
                             {{ item.label }}
