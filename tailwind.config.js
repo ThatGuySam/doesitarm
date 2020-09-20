08c8a684271a69f447f398e93f78728340bd55b9
Sam Carlton <sam@sam.lc>
2020-09-19 23:00:22 -0500

    Configure tailwind purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-    purge: false,
+    purge: {
+        enabled: process.env.NODE_ENV === 'production',
+        content: [
+            'components/**/*.vue',
+            'layouts/**/*.vue',
+            'pages/**/*.vue',
+            'plugins/**/*.js',
+            'nuxt.config.js'
+        ]
+    },
     future: {
         purgeLayersByDefault: true,
     },
