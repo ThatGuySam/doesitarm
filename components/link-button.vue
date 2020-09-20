bcc86105ec269efdaea1438a0ae7cead01aa71c6
Sam Carlton <sam@sam.lc>
2020-09-20 00:12:13 -0500

    Remove text-sm from link button

--- components/link-button.vue
+++ components/link-button.vue
@@ -4,7 +4,7 @@
         :href="href"
         :target="target"
         role="button"
-        class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-darker neumorphic-shadow hover:bg-indigo-400 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-600 active:bg-indigo-600 transition duration-150 ease-in-out"
+        class="relative inline-flex items-center px-4 py-2 border border-transparent leading-5 font-medium rounded-md text-white bg-darker neumorphic-shadow hover:bg-indigo-400 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-600 active:bg-indigo-600 transition duration-150 ease-in-out"
     >
         <slot />
     </a>
