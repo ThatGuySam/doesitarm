4ce21fa8909a9f020768fc8622bd61bf02aa1b41
Sam Carlton <sam@sam.lc>
2020-09-21 18:56:10 -0500

    Use rel=“noopener” on non-relative links

--- components/link-button.vue
+++ components/link-button.vue
@@ -3,6 +3,7 @@
     <a
         :href="href"
         :target="target"
+        :rel="rel"
         role="button"
         class="relative inline-flex items-center px-4 py-2 border border-transparent leading-5 font-medium rounded-md text-white bg-darker neumorphic-shadow hover:bg-indigo-400 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-600 active:bg-indigo-600 transition duration-150 ease-in-out"
     >
@@ -22,6 +23,13 @@ export default {
         target: {
             type: String,
             default: null
+        }
+    },
+    computed: {
+        rel () {
+            if (this.href.charAt(0) === '/') return null
+
+            return 'noopener'
         }
     }
 }
