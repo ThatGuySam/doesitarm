adb253d6980035a2a7654fa8987a850220f09860
Sam Carlton <sam@sam.lc>
2021-03-04 20:44:47 -0600

    Add either matches helper

--- helpers/matching.js
+++ helpers/matching.js
@@ -4,3 +4,27 @@ export function matchesWholeWord (needle, haystack) {
     return new RegExp('\\b' + needle + '\\b').test(haystack)
 }
 
+
+export function eitherMatches (stringA, stringB) {
+    const stringALength = stringA.length
+    const stringBLength = stringB.length
+
+    // If string lengths are equal
+    // then just compare the equality of the strings
+    if (stringALength === stringBLength) {
+        // console.log('Strings are equal length', stringA, stringB)
+        return (stringA === stringB)
+    }
+
+    // If string A is larger
+    // then find string B within it
+    if (stringALength > stringBLength) {
+        // console.log('String A is bigger', stringA, stringB)
+        return matchesWholeWord( stringB, stringA )
+    }
+
+    // If string B is larger
+    // then find string A within it
+    // console.log('String B is bigger', stringA, stringB)
+    return matchesWholeWord( stringA, stringB )
+}
