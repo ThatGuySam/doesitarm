9181ed8e136cd039b736eeff0349d61d813ae620
Sam Carlton <sam@sam.lc>
2021-05-03 15:48:41 -0500

    Add siteURL to runtime config

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
 
     publicRuntimeConfig: {
         allUpdateSubscribe: process.env.ALL_UPDATE_SUBSCRIBE,
-        testResultStore: process.env.TEST_RESULT_STORE
+        testResultStore: process.env.TEST_RESULT_STORE,
+        siteUrl: process.env.URL
     },
 
     /*
