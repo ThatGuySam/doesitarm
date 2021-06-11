5afe30d2f64d7480206486b580fad66a65251643
Sam Carlton <sam@sam.lc>
2021-06-11 16:31:22 -0500

    Add Netlify envirnment variables link

--- helpers/environment.js
+++ helpers/environment.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export const isProduction = process.env.NODE_ENV === 'PRODUCTION'
 
+// https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
 export const isNetlify = process.env.NETLIFY === true
 
 export const rootDir = path.resolve(__dirname, '../')
