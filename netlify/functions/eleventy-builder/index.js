6287833eeede44373771d4e494c6d761c7f2736f
Sam Carlton <sam@sam.lc>
2021-07-24 14:32:53 -0500

    Rename function file to eleventy-builder

--- netlify/functions/eleventy-builder/index.js
+++ netlify/functions/eleventy-builder/index.js
@@ -5,42 +5,39 @@ const { EleventyServerless } = require("@11ty/eleventy");
 require("./eleventy-bundler-modules.js");
 
 async function handler(event) {
+  let elev = new EleventyServerless("eleventy-builder", {
+    path: event.path,
+    query: event.queryStringParameters,
+    inputDir: ".",
+    functionsDir: "./netlify/functions/",
+  });
 
-    console.log('event.path', event.path)
-
-    let elev = new EleventyServerless("eleventy-builder", {
-        path: event.path,
-        query: event.queryStringParameters,
-        inputDir: ".",
-        functionsDir: "./netlify/functions/",
-    });
-
-    try {
-        return {
-            statusCode: 200,
-            headers: {
-                "Content-Type": "text/html; charset=UTF-8",
-            },
-            body: await elev.render(),
-        };
-    } catch (error) {
-        // Only console log for matching serverless paths
-        // (otherwise you’ll see a bunch of BrowserSync 404s for non-dynamic URLs during --serve)
-        if (elev.isServerlessUrl(event.path)) {
-            console.log("Serverless Error:", error);
-        }
-
-        return {
-            statusCode: error.httpStatusCode || 500,
-            body: JSON.stringify(
-                {
-                    error: error.message,
-                },
-                null,
-                2
-            ),
-        };
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/html; charset=UTF-8",
+      },
+      body: await elev.render(),
+    };
+  } catch (error) {
+    // Only console log for matching serverless paths
+    // (otherwise you’ll see a bunch of BrowserSync 404s for non-dynamic URLs during --serve)
+    if (elev.isServerlessUrl(event.path)) {
+      console.log("Serverless Error:", error);
     }
+
+    return {
+      statusCode: error.httpStatusCode || 500,
+      body: JSON.stringify(
+        {
+          error: error.message,
+        },
+        null,
+        2
+      ),
+    };
+  }
 }
 
 // Choose one:
@@ -48,7 +45,7 @@ async function handler(event) {
 // * Runs on first request only: Netlify On-demand Builder
 //   (don’t forget to `npm install @netlify/functions`)
 
-// exports.handler = handler;
+exports.handler = handler;
 
-const { builder } = require("@netlify/functions");
-exports.handler = builder(handler);
+//const { builder } = require("@netlify/functions");
+//exports.handler = builder(handler);
