9f7773b31dde78fa47428619e08f1cefad1b27f7
Sam Carlton <sam@sam.lc>
2021-09-11 14:52:56 -0500

    Add 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import Layout from '@/components/layout/default.js'
+
+function NotFound() {
+    return (
+        <Layout>
+            <div>Not Found</div>
+        </Layout>
+    )
+}
+
+export default NotFound
