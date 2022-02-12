61aefac86d69b032466964542fb1cb928d6a42b8
Sam Carlton <sam@sam.lc>
2022-02-12 12:38:53 -0600

    Try adding gtag plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -114,7 +114,9 @@ export default {
     /*
     ** Plugins to load before mounting the App
     */
-    plugins: [],
+    plugins: [
+        '@/plugins/gtag'
+    ],
 
     /*
     ** Nuxt.js modules
