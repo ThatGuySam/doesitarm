10e167952907bfa7761901817a8b8a5a4f1b989f
Sam Carlton <sam@sam.lc>
2022-02-27 14:29:12 -0600

    Add hello world cf function

---
+++ functions/hello-world.js
@@ -0,0 +1,13 @@
+export async function onRequest(context) {
+    // Contents of context object
+    const {
+        request, // same as existing Worker API
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+    } = context;
+
+    return new Response('Hello, worker!')
+}
