00cd98e13c19737cb031f85a2ade216b7c99083c
Sam Carlton <sam@sam.lc>
2022-03-06 14:07:47 -0600

    Add list end buttons component

---
+++ components/list-end-buttons.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex flex-col md:flex-row space-x-0 space-y-4 md:space-y-0 md:space-x-4">
+        <LinkButton
+            v-for="(link, i) in links"
+            :key="i"
+            :href="link.href"
+            class="text-xs"
+        >
+            {{ link.label }}
+        </LinkButton>
+    </div>
+</template>
+
+<script>
+
+import LinkButton from './link-button.vue'
+
+export default {
+    components: {
+        LinkButton,
+    },
+    props: {
+        query: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        links () {
+            return [
+                {
+                    label: 'Scan Your Own App',
+                    href: '/apple-silicon-app-test/'
+                },
+                {
+                    label: 'Request an App with Github',
+                    href: `https://github.com/ThatGuySam/doesitarm/issues?q=is%3Aissue+${ this.query }`
+                }
+            ]
+        }
+    }
+}
+
+</script>
