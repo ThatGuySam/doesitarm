58db0ccb30b4fc854ed087bb21c214f6b351aa69
Sam Carlton <sam@sam.lc>
2022-04-24 14:28:32 -0500

    Add carbon inline component

---
+++ components/carbon-inline.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="doesitarm-carbon-wrapper">
+        <component
+            id="_carbonads_js"
+
+            :is="'script'"
+            src="https://cdn.carbonads.com/carbon.js?serve=CK7DVK3M&placement=doesitarmcom"
+            type="text/javascript"
+            async
+            class="include-on-static"
+        />
+    </div>
+</template>
