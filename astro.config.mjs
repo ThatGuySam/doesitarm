1322ffad1d96514d365586b8d1f07cc0068399df
Sam Carlton <sam@sam.lc>
2022-04-25 22:26:42 -0500

    Configure Netlify Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify/functions'
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +10,9 @@ export default defineConfig({
     site: 'https://doesitarm.com',
 
     integrations: [
+        netlify({
+            dist: new URL('./dist/', import.meta.url)
+        }),
         vue(),
         tailwind()
     ]
