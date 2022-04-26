44844534159aa506ae0a5e968152a6e4c188d2a7
Sam Carlton <sam@sam.lc>
2022-04-26 11:46:38 -0500

    Add Astro Netlify Reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind'
+// Astro Netlify Reference
+// https://github.com/withastro/astro/tree/main/packages/integrations/netlify
 import netlify from '@astrojs/netlify/functions'
 
 
