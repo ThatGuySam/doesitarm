d37bf330955646cc8c55f0363e8214cff070f0bb
Sam Carlton <sam@sam.lc>
2022-05-08 10:57:16 -0500

    Add isDarwin helper

--- helpers/environment.js
+++ helpers/environment.js
@@ -4,3 +4,12 @@ import has from 'just-has'
 export function isNuxt( VueThis ) {
     return has( VueThis, [ '$nuxt' ])
 }
+
+// https://stackoverflow.com/a/8684009/1397641
+export function isDarwin () {
+    if ( typeof navigator !== 'undefined' ) return false
+
+    if ( typeof process === 'undefined' ) return false
+
+    return process.platform === 'darwin'
+}
