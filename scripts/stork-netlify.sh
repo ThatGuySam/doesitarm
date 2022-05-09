f72a795ebad6a52967a2b553f66313956ecf201f
Sam Carlton <sam@sam.lc>
2022-05-09 17:57:35 -0500

    Fix wrong format for linux

--- scripts/stork-netlify.sh
+++ scripts/stork-netlify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# curl https://files.stork-search.net/releases/latest/stork-amazon-linux -o stork-executable
-curl https://files.stork-search.net/releases/latest/stork-macos-latest -o stork-executable
+curl https://files.stork-search.net/releases/latest/stork-amazon-linux -o stork-executable
+# curl https://files.stork-search.net/releases/latest/stork-macos-latest -o stork-executable
 chmod +x stork-executable
 ./stork-executable build --input stork.toml --output search-index.st
