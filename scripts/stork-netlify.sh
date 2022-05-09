85f577470275ae0e5497cbb4e85e91dae6f0c735
Sam Carlton <sam@sam.lc>
2022-05-09 17:38:48 -0500

    Update for latest stork build

--- scripts/stork-netlify.sh
+++ scripts/stork-netlify.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://files.stork-search.net/releases/latest/stork-ubuntu-latest
+curl https://files.stork-search.net/releases/latest/stork-ubuntu-latest -o stork-ubuntu-latest
+# curl https://files.stork-search.net/releases/latest/stork-macos-latest -o stork-ubuntu-latest
 chmod +x stork-ubuntu-latest
-./stork-ubuntu-latest --build static/stork.toml
+./stork-ubuntu-latest build --input static/stork.toml --output static/search-index.st
