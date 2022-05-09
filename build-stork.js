d0cae3fc1b80cc82e8017e6bf5ddae34e63cac96
Sam Carlton <sam@sam.lc>
2022-05-09 16:50:50 -0500

    Download stork executables on netlify

--- build-stork.js
+++ build-stork.js
@@ -2,19 +2,20 @@ import fs from 'fs-extra'
 
 import {
     downloadStorkExecutable,
-    writeStorkToml
+    // writeStorkToml
 } from './helpers/stork/toml.js'
 
 ;(async () => {
 
+    console.log( 'Downloading Stork executable...' )
     await downloadStorkExecutable()
 
-    console.log( 'Building Stork index TOML...' )
+    // console.log( 'Building Stork index TOML...' )
 
     // Get Sitemap Endpoints JSON
-    const sitemap = await fs.readJson( './static/sitemap-endpoints.json' )
+    // const sitemap = await fs.readJson( './static/sitemap-endpoints.json' )
 
-    await writeStorkToml( sitemap )
+    // await writeStorkToml( sitemap )
 
     // From here we hand off to the Stork executable
 
