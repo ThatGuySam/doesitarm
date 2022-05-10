df7ea9c6a6e12209b8be230904187e0ba2d7bf89
Sam Carlton <sam@sam.lc>
2022-05-10 11:56:54 -0500

    Try v1.4.2

--- scripts/stork-netlify.sh
+++ scripts/stork-netlify.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-curl https://files.stork-search.net/releases/latest/stork-amazon-linux -o stork-executable
-# curl https://files.stork-search.net/releases/latest/stork-macos-latest -o stork-executable
+# curl https://files.stork-search.net/releases/latest/stork-amazon-linux -o stork-executable
+# curl https://files.stork-search.net/releases/v1.4.3/stork-macos-latest -o stork-executable
+
+curl https://files.stork-search.net/releases/v1.4.2/stork-amazon-linux -o stork-executable
+# curl https://files.stork-search.net/releases/v1.4.2/stork-macos-10-15 -o stork-executable
+
 chmod +x stork-executable
 ./stork-executable build --input stork.toml --output search-index.st
