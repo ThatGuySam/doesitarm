e1c1cdddfd45e877213fc73ec7815f9b2072f19f
Sam Carlton <sam@sam.lc>
2022-05-10 13:19:30 -0500

    Move download and indexing to it’s own file

--- helpers/stork/toml.js
+++ helpers/stork/toml.js
@@ -4,70 +4,17 @@ import has from 'just-has'
 import TOML from '@iarna/toml'
 
 
-import { isDarwin } from '~/helpers/environment.js'
 import {
     isNonEmptyString,
     isNonEmptyArray
 } from '~/helpers/check-types.js'
 import {
-    storkVersion,
-    storkExecutableName,
-    storkExecutablePath,
     storkTomlPath,
 } from '~/helpers/stork/config.js'
+import { downloadStorkExecutable } from '~/helpers/stork/executable.js'
 
 
-// https://stork-search.net/docs/install
-const execDownloadUrls = {
-    darwin: `https://files.stork-search.net/releases/v${ storkVersion }/stork-macos-10-15`,
-    default: `https://files.stork-search.net/releases/v${ storkVersion }/stork-ubuntu-20-04`
-    // default: `https://files.stork-search.net/releases/v${ storkVersion }/stork-amazon-linux`
-}
 
-export async function downloadStorkExecutable () {
-    const envKey = isDarwin() ? 'darwin' : 'default'
-
-    const execDownloadUrl = execDownloadUrls[ envKey ]
-
-    // console.log( 'execDownloadUrl', execDownloadUrl )
-
-    // Delete any existing executable
-    // so we don't get write errors
-    // or false positives from preexisting executable files
-    await fs.remove( storkExecutablePath )
-
-    // Download the binary
-    await execa( `curl`, [
-        execDownloadUrl,
-
-        // Set filename
-        '-o',
-        storkExecutableName
-    ])
-
-
-    // Set the downloaded binary as executable
-    await fs.chmod( storkExecutablePath, '755' )
-    // Check that our downloaded binary is executable
-    // https://stackoverflow.com/a/69897809/1397641
-    const stats = await fs.stat( storkExecutablePath )
-    const isExecutable = !!(stats.mode & fs.constants.S_IXUSR)
-
-    // console.log( 'isExecutable', isExecutable )
-    if ( !isExecutable ) throw new Error( 'Downloaded binary is not executable.' )
-
-
-    // Check Stork version
-    // so we know our binary is working
-    const { stdout } = await execa( storkExecutablePath, [
-        '--version'
-    ])
-
-    console.log( 'Stork Version', stdout )
-    if ( !stdout.includes( storkVersion ) ) throw new Error( 'Stork --version command failed.' )
-
-    return stdout
-}
 
 function makeIndexContentsFromListing ( listing ) {
 
