989e53faed14126890b91c96592372c470aa03ee
Sam Carlton <sam@sam.lc>
2022-05-15 11:51:33 -0500

    Fix binary reference

--- helpers/stork/config.js
+++ helpers/stork/config.js
@@ -7,7 +7,7 @@ export const storkVersion = '1.4.2'
 export const storkExecutableName = storkOptions.executable
 export const storkExecutablePath = `./${ storkExecutableName }`
 export const storkTomlPath = storkOptions.toml
-export const storkIndexPath = storkOptions.binary
+export const storkIndexPath = storkOptions.index
 
 export const storkIndexRelativeURL = storkIndexPath.replace('static/', '')
 export const storkScriptURL = `https://files.stork-search.net/releases/v${ storkVersion }/stork.js`
