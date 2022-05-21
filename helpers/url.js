d96332730d47f39b3f89b733ba0e464048e82386
Sam Carlton <sam@sam.lc>
2022-05-21 11:25:31 -0500

    Add getApiUrl helper

--- helpers/url.js
+++ helpers/url.js
@@ -1,3 +1,4 @@
+
 export function getSiteUrl () {
 
     // console.log( 'import.meta.site', import.meta.env )
@@ -36,6 +37,25 @@ export function getSiteUrl () {
     }
 
     throw new Error('Could not find site URL')
+}
+
+export function getApiUrl () {
+
+    const hasImportMeta = typeof import.meta !== 'undefined'
+    const hasImportMetaEnv = hasImportMeta && typeof import.meta.env !== 'undefined'
+
+    // Try PUBLIC_API_DOMAIN
+    if ( typeof process.env.PUBLIC_API_DOMAIN !== 'undefined' ) {
+        // console.log('Has env.PUBLIC_API_DOMAIN')
+        return process.env.PUBLIC_API_DOMAIN
+    }
+
+    if ( hasImportMetaEnv && typeof import.meta.env.PUBLIC_API_DOMAIN !== 'undefined' ) {
+        // console.log('Has PUBLIC_API_DOMAIN')
+        return import.meta.env.PUBLIC_API_DOMAIN
+    }
+
+    throw new Error('Could not find API URL')
 }
 
 export function getPartPartsFromUrl ( urlString ) {
