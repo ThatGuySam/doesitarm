27b95b8cae75dfe9756212772e5f912d6e90fc8b
Sam Carlton <sam@sam.lc>
2022-06-05 15:25:43 -0500

    Setup sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import tailwind from '@astrojs/tailwind'
 // Astro Netlify Reference
 // https://github.com/withastro/astro/tree/main/packages/integrations/netlify
 import netlify from '@astrojs/netlify/functions'
-// import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
+import sitemap from '@astrojs/sitemap'
 
+// import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 
 import { makeViteDefinitions } from './helpers/public-runtime-config.mjs'
 
@@ -13,32 +14,34 @@ import { makeViteDefinitions } from './helpers/public-runtime-config.mjs'
 export default defineConfig({
     publicDir: './static',
     site: 'https://doesitarm.com',
-
     integrations: [
         netlify({
             dist: new URL('./dist/', import.meta.url)
         }),
         // Astro Vue Reference
+        // https://github.com/withastro/astro/blob/main/packages/integrations/sitemap/src/index.ts
         // https://github.com/withastro/astro/tree/main/packages/integrations/vue
         vue(),
-        tailwind()
+        tailwind(),
+        // Sitemap Reference
+        // https://github.com/withastro/astro/tree/main/packages/integrations/sitemap#configuration
+        sitemap()
     ],
-
     // Vite options
     // https://docs.astro.build/en/reference/configuration-reference/#vite
     vite: {
         // Vite: https://vitejs.dev/config/#define
         // esbuild: https://esbuild.github.io/api/#define
         define: {
-            ...makeViteDefinitions(),
+            ...makeViteDefinitions()
         },
         // plugins: [
         //     viteCommonjs()
         // ],
         build: {
             commonjsOptions: {
-                transformMixedEsModules: true,
-            },
+                transformMixedEsModules: true
+            }
         }
     }
 })
