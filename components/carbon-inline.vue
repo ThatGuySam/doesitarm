562609abc9b86904c3d4c4b28f23c387aa3b05a6
Sam Carlton <sam@sam.lc>
2022-06-10 12:38:57 -0500

    Clean up carbon ads component

--- components/carbon-inline.vue
+++ components/carbon-inline.vue
@@ -1,14 +1,12 @@
 <template>
     <div class="doesitarm-carbon-wrapper">
         <component
+            :is="'script'"
             v-once
             id="_carbonads_js"
-
-            :is="'script'"
             src="https://cdn.carbonads.com/carbon.js?serve=CK7DVK3M&placement=doesitarmcom"
             type="text/javascript"
             async
-            class="include-on-static"
         />
     </div>
 </template>
