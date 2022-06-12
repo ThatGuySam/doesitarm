beb2e23e8e873d7c9ae57927e2de9d181eba971c
Sam Carlton <sam@sam.lc>
2022-06-12 11:39:00 -0500

    Add gaMeasurementId constant

--- helpers/constants.js
+++ helpers/constants.js
@@ -4,3 +4,6 @@ export const filterSeparator = '_'
 export const sitemapLocation = './static/'
 
 export const sitemapIndexFileName = 'sitemap-index.xml'
+
+// https://analytics.google.com/analytics/web/#/a28434239p302384837/admin/streams/table/3228170828
+export const gaMeasurementId = 'G-0WLH5YTTB0'
