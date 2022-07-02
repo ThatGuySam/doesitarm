2b85f8b80d29781bdc7233724eb3dbd63a256458
Sam Carlton <sam@sam.lc>
2022-07-02 13:40:15 -0500

    Configure DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -117,5 +117,20 @@ module.exports = {
     variants: {
         backgroundImage: ['responsive', 'hover']
     },
-    plugins: []
+    plugins: [
+        require( 'daisyui' )
+    ],
+
+    // daisyUI config
+    // https://daisyui.com/docs/config/
+    daisyui: {
+        // styled: false,
+        // themes: false,
+        // base: false,
+        // utils: false,
+        // logs: true,
+        // rtl: false,
+        prefix: 'daisy',
+        darkTheme: 'dark',
+    },
 }
