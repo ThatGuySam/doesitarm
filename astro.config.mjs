3ffb32a0028cc386f2195430b0a17c8f87ac621b
Sam Carlton <sam@sam.lc>
2022-07-26 23:08:16 -0500

    Set output as server

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ console.log( 'Running Astro Config File' )
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
     publicDir: './static',
     site: 'https://doesitarm.com',
     integrations: [
