7c10c0a7043a4d1aa45c2bd12bf91e06eed7fa68
Sam Carlton <sam@sam.lc>
2022-08-21 09:07:42 -0500

    Try different port

--- scripts/scan-new-apps.js
+++ scripts/scan-new-apps.js
@@ -3,18 +3,23 @@ import axios from 'axios'
 
 import viteConfig from '~/vite.config.mjs'
 
+const port = 1337
+
 ;(async () => {
     // await scanNewAppsAsBrowser()
     // http://localhost:3000/
     const server = await createServer({
         ...viteConfig,
+        port
     })
+
+    console.log( 'server', server )
 
     await server.listen();
 
-    console.log('Server listening on http://localhost:3000/')
+    console.log(`Server listening on http://localhost:${ port }/`)
 
-    const { data } = await axios.get('http://localhost:3000/')
+    const { data } = await axios.get(`http://localhost:${ port }/`)
 
     console.log( data.slice(0, 100) )
 
