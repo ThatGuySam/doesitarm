4c39ace22beefbbc7aee46c459a55d40c419bf0e
Sam Carlton <sam@sam.lc>
2022-08-21 10:33:29 -0500

    Log globals and this

--- scripts/vercel-post-deploy/index.js
+++ scripts/vercel-post-deploy/index.js
@@ -1,7 +1,13 @@
 
 
-;(async () => {
+const rootThis = this
+
+;(async function () {
     console.log( 'process.env', process.env )
+    console.log( 'global', global )
+    console.log( 'globalThis', globalThis )
+
+    console.log( 'rootThis', rootThis )
 
     process.exit()
 })()
