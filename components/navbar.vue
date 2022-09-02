0e6363d83fcfa6fe56727aaec4bfafeac82880c9
j5ive <johned.matt@gmail.com>
2022-09-01 20:14:37 -0500

    Added icon emojis for nav, merged 'Home' nav item with arm logo, added container to nav ad, improved responsiveness

--- components/navbar.vue
+++ components/navbar.vue
@@ -3,7 +3,8 @@
     <nav
         :class="[
             'fixed top-0 left-0 right-0 flex z-navbar',
-            'bg-gradient-to-bl from-dark to-darker bg-fixed'
+            'bg-gradient-to-bl from-dark to-darker bg-fixed',
+            'justify-center items-center items-baseline'
         ]"
     >
         <div class="mobile-menu-container flex items-center lg:hidden p-2">
@@ -52,7 +53,7 @@
         <div
             id="mobile-menu"
             :class="[
-                'mobile-menu hidden target:visible lg:hidden absolute bg-blur top-0 left-0 right-0 w-full py-3 px-2',
+                'mobile-menu hidden target:visible lg:hidden absolute bg-blur top-0 left-0 right-0 w-full py-3 px-2 pr-0',
             ]"
         >
             <!-- Mobile menu button -->
@@ -80,7 +81,13 @@
                 </svg>
             </a>
 
-            <div class="px-2 pt-2 pb-3 lg:px-3">
+            <div class="px-2 pt-2 pb-3 lg:px-3 md:pt-5">
+                <a
+                    href="/"
+                    class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out text-gray-300 hover:bg-gray-700"
+                >
+                    🦾 Home
+                </a>
                 <a
                     v-for="(item, index) in items"
                     :key="index"
@@ -90,36 +97,56 @@
                         (currentPath === item.url) ? 'text-white bg-gray-900 hover:text-white' : 'text-gray-300 hover:bg-gray-700'
                     ]"
                 >
+                    {{ item.emoji }}
                     {{ item.label }}
                 </a>
             </div>
             <hr>
         </div>
 
-        <div class="reponsive-menu-container relative w-full max-w-7xl mx-auto lg:px-6">
-            <div class="flex justify-between h-16">
-                <div class="flex">
-                    <div class="flex-shrink-0 flex items-center text-4xl lg:text-5xl py-3">
-                        <div>🦾</div>
-                    </div>
-                    <div class="hidden lg:ml-6 lg:flex lg:items-center space-x-4">
-                        <a
-                            v-for="(item, index) in items"
-                            :key="index"
-                            :href="item.url"
-                            :class="[
-                                'px-3 py-2 rounded-md text-sm font-medium leading-5 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out',
-                                (currentPath === item.url) ? 'text-white bg-darker hover:text-white neumorphic-shadow' : 'text-gray-300 hover:bg-darker hover:neumorphic-shadow'
-                            ]"
+        <div class="reponsive-menu-container relative w-full max-w-7xl ml-0 mb-1 mt-1 lg:mx-2 md:mr-2 md:mt-2">
+            <div class="flex justify-between">
+                <!-- responsive logo -->
+                <div class="flex-shrink-0 flex items-center">
+                    <a
+                        href="/"
+                        class="group h-full w-full lg:w-[110px] lg:h-[80px] lg:hover:bg-darker hover:neumorphic-shadow-logo lg:px-3 py-2 lg:rounded-md focus:outline-none lg:focus:text-white lg:focus:bg-gray-700 lg:leading-tight lg:transition lg:duration-150 lg:ease-in-out text-center whitespace-nowrap"
+                    >
+                        <div
+                            class="lg:text-5xl text-3xl md:text-4xl lg:group-hover:text-xl"
                         >
-                            {{ item.label }}
-                        </a>
-                    </div>
+                            🦾
+                        </div>
+                        <div
+                            class="hidden lg:group-hover:block text-gray-300 text-sm font-medium leading-4"
+                        >
+                            Home
+                        </div>
+                    </a>
                 </div>
 
-                <div class="flex items-center">
-                    <div class="flex-shrink-0">
-                        <!-- <span class="rounded-md shadow-sm">
+                <!-- nav items desktop view -->
+                <div class="hidden lg:flex lg:items-center grow justify-evenly">
+                    <a
+                        v-for="(item, index) in items"
+                        :key="index"
+                        :href="item.url"
+                        :class="[
+                            'px-3 py-2 rounded-md text-sm font-medium focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out text-center leading-tight whitespace-nowrap',(currentPath === item.url) ? 'text-white bg-darker hover:text-white neumorphic-shadow' : 'text-gray-300 hover:bg-darker hover:neumorphic-shadow'
+                        ]"
+                    >
+                        <div class="text-xl">
+                            {{ item.emoji }}
+                        </div>
+                        <div>
+                            {{ item.label }}
+                        </div>
+                    </a>
+                </div>
+
+                <!-- advertisement -->
+                <div class="flex shrink items-center">
+                    <!-- <span class="rounded-md shadow-sm">
                             <LinkButton
                                 href="https://prf.hn/l/daWX1vk"
                                 class="relative inline-flex items-center border-indigo-500"
@@ -138,18 +165,15 @@
 
                         </span> -->
 
-                        <a
-                            :class="[
-                                'underline px-3 py-2 rounded-md text-xs font-medium leading-5 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out',
-                                //(currentPath === item.url) ? 'text-white bg-darker hover:text-white neumorphic-shadow' : 'text-gray-300 hover:bg-darker hover:neumorphic-shadow'
-                            ]"
-                            href="https://prf.hn/l/daWX1vk"
-                        >
-                            🅿️ Windows on ARM now works on Parallels
-                        </a>
-                    </div>
+                    <a
+                        :class="[
+                            'underline px-3 py-2 rounded-md text-xs font-medium focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out bg-black ml-4 m-1',
+                        ]"
+                        href="https://prf.hn/l/daWX1vk"
+                    >
+                        🅿️ Windows on ARM now works on Parallels
+                    </a>
                 </div>
-
             </div>
         </div>
         <!--
@@ -158,7 +182,7 @@
             Menu open: "block", Menu closed: "hidden"
         -->
     </nav>
-
+    
 </template>
 
 <script>
@@ -176,31 +200,33 @@ export default {
             type: Array,
             default: () => ([
                 {
-                    label: 'Home',
-                    url: '/',
-                },
-                {
+                    emoji: '🤖',
                     label: 'Categories',
                     url: '/categories',
                 },
                 {
+                    emoji: '💻',
                     label: 'Devices',
                     url: '/devices',
                 },
                 {
+                    emoji: '🚀',
                     label: 'Benchmarks',
                     url: '/benchmarks',
                 },
                 {
+                    emoji: '🍺',
                     label: 'Homebrew',
                     url: '/kind/homebrew',
                 },
                 {
+                    emoji: '🎮',
                     label: 'Games',
                     url: '/games',
                 },
                 {
-                    label: 'Apple Silicon App Test',
+                    emoji: '🧪',
+                    label: 'App Test',
                     url: '/apple-silicon-app-test',
                 },
             ])
