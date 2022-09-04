350f9d5b19e83633e048c0da56cfcb3d4e9b0814
Sam Carlton <sam@sam.lc>
2022-09-04 10:13:37 -0500

    Add isLinux helper

--- helpers/environment.js
+++ helpers/environment.js
@@ -5,11 +5,37 @@ export function isNuxt( VueThis ) {
     return has( VueThis, [ '$nuxt' ])
 }
 
-// https://stackoverflow.com/a/8684009/1397641
-export function isDarwin () {
-    if ( typeof navigator !== 'undefined' ) return false
+export function isBrowserContext () {
+    if ( typeof navigator === 'undefined' ) return false
 
+    return true
+}
+
+export function hasProcesGlobal () {
     if ( typeof process === 'undefined' ) return false
 
+    return true
+}
+
+
+// https://stackoverflow.com/a/8684009/1397641
+export function isDarwin () {
+    if ( isBrowserContext() ) return false
+
+    if ( !hasProcesGlobal() ) return false
+
     return process.platform === 'darwin'
+}
+
+
+export function isLinux () {
+    if ( isBrowserContext() ) return false
+
+    if ( !hasProcesGlobal() ) return false
+
+    if ( process.platform === 'linux' ) return true
+
+    if ( process.platform === 'openbsd' ) return true
+
+    return false
 }
