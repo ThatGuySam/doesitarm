4cd4a9edc6affd39f358760799e3fdfdfb51fefe
Sam Carlton <sam@sam.lc>
2022-09-04 11:43:05 -0500

    Use import for execSync

--- scripts/vercel-build.js
+++ scripts/vercel-build.js
@@ -1,4 +1,4 @@
-const { execSync } = require('child_process')
+import { execSync } from 'child_process' // The exec import
 
 ;(async () => {
     // scan-new-apps test-prebuild build-lists-and-api test-postbuild-api
