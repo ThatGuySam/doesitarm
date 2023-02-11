8aad6b79450b0dd9ffa218784f58827e5bf28713
Sam Carlton <sam@sam.lc>
2023-02-11 17:22:12 -0600

    Only rurnn post-deploy on macos

--- scripts/vercel-post-deploy/index.js
+++ scripts/vercel-post-deploy/index.js
@@ -1,6 +1,14 @@
+import { isMacOS } from 'std-env'
+
 import axios from 'axios'
 
 ;(async () => {
+
+    if ( !isMacOS ) {
+        console.log('Not on macOS, skipping')
+
+        process.exit()
+    }
 
     const { data } = await axios.get(`https://master--doesitarm.netlify.app/apple-silicon-app-test`)
 
